327b0cca9f2c319824ddd0609673d7f286506ad2
pedrodavid1697 <260624093+pedrodavid1697@users.noreply.github.com>
2026-09-17 21:01:44 +0530

    Add DeveloperProfile example for backend roadmap

--- Day-01/DeveloperProfile.java
+++ Day-01/DeveloperProfile.java
@@ -6,5 +6,17 @@ public class DeveloperProfile {
         System.out.println("Goal : Java Backend developer");
         System.out.println("Language : Java");
         System.out.println("Day : 1");
+        System.out.println("Roadmap to backend");
+        System.out.println("1. Java ");
+        System.out.println("2. SQL ");
+        System.out.println("3. Spring Boot ");
+        System.out.println("4. REST APIs ");
+        System.out.println("5. PostgreSQL ");
+        System.out.println("6. Redis ");
+        System.out.println("7. Kafka ");
+        System.out.println("8. Docker ");
+        System.out.println("9. AWS ");
+        System.out.println("10. Kubernetes ");
+
     }
 }
