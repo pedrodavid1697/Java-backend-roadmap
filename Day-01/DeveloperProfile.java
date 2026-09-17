caffd67f33aa6319a8b4743ea4a480b9bd69addb
pedrodavid1697 <260624093+pedrodavid1697@users.noreply.github.com>
2026-09-17 07:37:02 +0530

    Add DeveloperProfile example

---
+++ Day-01/DeveloperProfile.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class DeveloperProfile {
+    public static void main(String[] args) {
+        System.out.println("Name : David Pedro");
+        System.out.println("Goal : Java Backend developer");
+        System.out.println("Language : Java");
+        System.out.println("Day : 1");
+    }
+}
