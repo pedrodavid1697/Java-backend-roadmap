7ce80500b53fdc838a553671c88388b8b1e5e800
pedrodavid1697 <260624093+pedrodavid1697@users.noreply.github.com>
2026-09-18 09:23:50 +0530

    Add Day2 file code modified

--- Day-02/Day2Variables.java
+++ Day-02/Day2Variables.java
@@ -2,91 +2,91 @@ import jdk.dynalink.linker.ConversionComparator;
 
 public class Day2Variables {
     public static void main(String[] args) {
-//        String name = "David";
-//        int age = 26;
-//        float height = 5.4f;
-//        boolean employed = true;
-//        char grade ='A';
-//
-//        System.out.println("Name : "+name);
-//        System.out.println("Age : "+age);
-//        System.out.println("Height : "+height);
-//        System.out.println("Employed : "+employed);
-//        System.out.println("Grade : "+grade);
+        /*String name = "David";
+        int age = 26;
+        float height = 5.4f;
+        boolean employed = true;
+        char grade ='A';
 
-//        Basic Calculation
-//                int a = 25;
-//                int b = 7;
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("Height : "+height);
+        System.out.println("Employed : "+employed);
+        System.out.println("Grade : "+grade); */
 
-//        System.out.println("a+b : "+(a+b)); //32
-//        System.out.println("a-b : "+(a-b)); //18
-//        System.out.println("a*b : "+(a*b)); //175
-//        System.out.println("a/b : "+(a/b)); //3
-//        System.out.println("a%b : "+(a%b)); //4
+      /*  Basic Calculation
+                int a = 25;
+                int b = 7;
 
-//        Salary
-//        double monthly_salary = 30000d;
-//        double annual_salary = (monthly_salary*12);
-//
-//        System.out.println("Monthly Salary : "+monthly_salary);
-//        System.out.println("Annual Salary : "+annual_salary);
+        System.out.println("a+b : "+(a+b)); //32
+        System.out.println("a-b : "+(a-b)); //18
+        System.out.println("a*b : "+(a*b)); //175
+        System.out.println("a/b : "+(a/b)); //3
+        System.out.println("a%b : "+(a%b)); //4 */
 
-//        Rectangle
-//                int length = 10;
-//                int width = 5;
-//
-//        System.out.println("Area of Rectangle is : "+length*width);
-//        System.out.println("Perimeter of Rectangle is : "+ 2*(length+width));
+       /* Salary
+        double monthly_salary = 30000d;
+        double annual_salary = (monthly_salary*12);
 
-//        Temperature
-//                double celsius = 30d;
-//                double fahrenheit = (celsius * 9/5) + 32;
-//
-//        System.out.println("Celsius is : "+celsius);
-//        System.out.println("Fahrenheit is : "+fahrenheit);
+        System.out.println("Monthly Salary : "+monthly_salary);
+        System.out.println("Annual Salary : "+annual_salary); */
 
+        /* Rectangle
+                int length = 10;
+                int width = 5;
 
-//        Type Conversion
-//                int number = 100;
-//                double new_number = number; //widening conversion
-//        System.out.println("Number is : "+number);
-//        System.out.println("New number is : "+new_number);
-//
-//                double decimal = 99.99;
-//                int new_decimal = (int) decimal; //Narrowing conversion
-//
-//        System.out.println("Decimal is : "+decimal);
-//        System.out.println("New decimal is : "+new_decimal);
+        System.out.println("Area of Rectangle is : "+length*width);
+        System.out.println("Perimeter of Rectangle is : "+ 2*(length+width)); */
 
-//                <------Comparison ------>
-//                        int age = 25;
-//        System.out.println(age > 18);
-//        System.out.println(age == 25);
-//        System.out.println(age < 30);
-//        System.out.println(age!= 20);
+        /* Temperature
+                double celsius = 30d;
+                double fahrenheit = (celsius * 9/5) + 32;
 
-//            <----------Logical operators------->
-//                        int age = 25;
-//                        boolean hasDrivingLicense = true;
-//
-//        System.out.println("Can David drive : "+ (age > 18 && hasDrivingLicense == true ));
-
-//                <----------Shopping Calculation------->
-//                            int price = 500;
-//                            int quantity = 3;
-//                            int PurchasePrice = price * quantity ;
-//
-//
-//        System.out.println("Purchase Price : " + PurchasePrice);
-//        System.out.println("After 10% Discount on Purchase Price");
-//
-//        double discountAmount = (PurchasePrice*10) / 100;
-//        double finalPrice = PurchasePrice - discountAmount;
-//
-//        System.out.println("Final Price : "+ finalPrice);
+        System.out.println("Celsius is : "+celsius);
+        System.out.println("Fahrenheit is : "+fahrenheit); */
 
 
-//                    <------- Mini Challenge Student Records------->
+        /* Type Conversion
+                int number = 100;
+                double new_number = number; //widening conversion
+        System.out.println("Number is : "+number);
+        System.out.println("New number is : "+new_number);
+
+                double decimal = 99.99;
+                int new_decimal = (int) decimal; //Narrowing conversion
+
+        System.out.println("Decimal is : "+decimal);
+        System.out.println("New decimal is : "+new_decimal); */
+
+               /* <------Comparison ------>
+                        int age = 25;
+        System.out.println(age > 18);
+        System.out.println(age == 25);
+        System.out.println(age < 30);
+        System.out.println(age!= 20); */
+
+            /* <----------Logical operators------->
+                        int age = 25;
+                        boolean hasDrivingLicense = true;
+
+        System.out.println("Can David drive : "+ (age > 18 && hasDrivingLicense == true )); */
+
+                /* <----------Shopping Calculation------->
+                            int price = 500;
+                            int quantity = 3;
+                            int PurchasePrice = price * quantity ;
+
+
+        System.out.println("Purchase Price : " + PurchasePrice);
+        System.out.println("After 10% Discount on Purchase Price");
+
+        double discountAmount = (PurchasePrice*10) / 100;
+        double finalPrice = PurchasePrice - discountAmount;
+
+        System.out.println("Final Price : "+ finalPrice); */
+
+
+                   /*  <------- Mini Challenge Student Records------->
                             int maths = 85;
                             int english = 92;
                             int physics = 78;
@@ -97,7 +97,7 @@ public class Day2Variables {
         System.out.println("English : " + english);
         System.out.println("Physics : " + physics);
         System.out.println("Total : "+ total);
-        System.out.println("Percentage : " + percentage);
+        System.out.println("Percentage : " + percentage); */
 
     }
 }
